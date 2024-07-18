69ff2886af29603f29a00247ef1b88b69bdc8821
GoodBoyNYC <5098496+GoodBoyNYC@users.noreply.github.com>
2024-07-17 21:08:36 -0400

    feat: Refactor calculator functions and add display
    Refactor the calculator functions to handle an operand, num1, and num2 as parameters. Also, add a display element to the HTML file with a default value of '0000'.

--- calc.js
+++ calc.js
@@ -7,17 +7,59 @@ so start by creating functions for the following items and testing them in your 
 -  multiply
 -  divide
 */
-//All wrong. Assumed functions were just 2 variables.
-// function add(num1,num2) {
-//     return num1+num2;
-// };
-// function subtract(num1, num2){
-//     return num1-num2;
-// };
-// function multiply(num1,num2){
-//     return num1*num2;
-// };
-// function divide(num1,num2){
-//     return num1/num2;
-// };
 
+let operand = ''
+let num1 = 0;
+let num2 = 0;
+
+function operand(operand,num1,num2){
+    if(operand=='+'){
+        return num1+num2;
+    }
+    else if(operand=='-'){
+        return num1-num2;
+    }
+    else if(operand=='*'){
+        return num1*num2;
+    }
+    else if(operand=='/'){
+        return num1/num2;
+    }
+};
+
+let gridSize = 20;
+const container = document.querySelector('.container');
+const btnReset = document.querySelector('.reset');
+const btnGridSize = document.querySelector('.gridSize');
+let gridArray = [];
+
+
+function createGrid(gridSize) {
+    container.innerHTML = ''
+    for (let x = 0; x < gridSize; x++) {
+        let row = document.createElement("div");
+        container.appendChild(row);
+        row.classList.add("row");
+        for (let y = 0; y < gridSize; y++) {
+            let cell = document.createElement("div");
+            cell.classList.add("cell");
+            row.appendChild(cell);
+        }
+    }
+    gridArray = document.getElementsByClassName("cell");
+    Array.from(gridArray).forEach((cell) => {
+        cell.addEventListener("mouseover", event => {
+            cell.style.backgroundColor = 'grey';
+        });
+    });
+}
+btnReset.addEventListener("click", event => {
+    createGrid(gridSize);
+});
+
+btnGridSize.addEventListener("click", event => {
+    gridSize=prompt('What size');
+    createGrid(gridSize);
+});
+
+createGrid(gridSize);
