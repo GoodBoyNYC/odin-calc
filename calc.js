1c2ee34a0ca5e5cfb17519df4041c659ccab6e66
GoodBoyNYC <5098496+GoodBoyNYC@users.noreply.github.com>
2024-07-19 23:33:14 -0400

    chore: Refactor calculator functions, add display, and update button types
    Refactor calculator functions to handle operands and add a display element. Update button types to improve functionality. Fix type error on display.

--- calc.js
+++ calc.js
@@ -1,47 +1,74 @@
-/*
---1--
-Your calculator is going to contain functions for all of the basic math operators you typically find on calculators, 
-so start by creating functions for the following items and testing them in your browser’s console.
--  add
--  subtract
--  multiply
--  divide
-*/
-
-
 let display = document.querySelector(".display");
-let dispArr = [0,0,0,0,0,0,0,0];
-
-const btnNum = document.querySelectorAll(".num");
-let operan = ''
+let dispArr = [0, 0, 0, 0, 0, 0, 0, 0];
 let num1 = 0;
 let num2 = 0;
-function updateDisplay(val){
+const btnNum = document.querySelectorAll(".calcBtn");
+let operand = ''
+
+function updateDisplay(val) {
     dispArr.shift();
     dispArr.push(val);
-    display.textContent=dispArr.join("");
+    display.textContent = dispArr.join("");
+};
+function clearDisplay() {
+    dispArr = [0, 0, 0, 0, 0, 0, 0, 0];
+    display.textContent = dispArr.join("");
+}
+function reset(){
+    num1=0;
+    num2=0;
+    operand='';
+    clearDisplay();
+}
+function dispToNum() {
+    if(operand==''){
+        num1 = Number(dispArr.join(""));
+    } else{
+        num2 = Number(dispArr.join(""));
+    }
+    clearDisplay();
+};
+function result(val){
+    dispArr.textContent=val;
+};
+
+function operate(op,num1,num2){
+    let answer=0;
+    if(operand=='+'){
+        answer=num1+num2;
+    }
+    else if(operand=='-'){
+        answer=num1-num2;
+    }
+    else if(operand=='x'){
+        answer=num1*num2;
+    }
+    else if(operand=='/'){
+        answer=num1/num2;
+    }
+    else if(operand=='='){
+        
+    }
+    result(answer);
 };
 btnNum.forEach(btn => {
-    btn.addEventListener("click",event => {
-        updateDisplay(btn.textContent);
+    btn.addEventListener("click", event => {
+        if (btn.classList.contains('clear')) {
+            clearDisplay();
+        }
+        else if (btn.classList.contains("num")) {
+            updateDisplay(btn.textContent);
+        }
+        else if (btn.classList.contains("operand")) {
+            //push current display of digits to num1
+            dispToNum();
+            operand=btn.textContent;
+            if(btn.textContent=='='){
+                operate(operand,num1,num2);
+            }
+        }
+        console.log(num1);
+        console.log(num2);
+        console.log(operand);
     })
 });
-
-
-
-
-console.log('dogs');
-// function operate(operand,num1,num2){
-//     if(operand=='+'){
-//         return num1+num2;
-//     }
-//     else if(operand=='-'){
-//         return num1-num2;
-//     }
-//     else if(operand=='*'){
-//         return num1*num2;
-//     }
-//     else if(operand=='/'){
-//         return num1/num2;
-//     }
-// };
